fd50edd4287bc55bba65e444fb0ca039bd056011
narendarputta <79365242+narendarputta@users.noreply.github.com>
2021-02-24 16:25:48 +0530

    Update settings.py

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -87,17 +87,17 @@ WSGI_APPLICATION = 'firstproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'product',
+#         'USER' : 'postgres',
+#         'PASSWORD' :  '1234',
+#         'HOST':'localhost'
+#     }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'product',
-        'USER' : 'postgres',
-        'PASSWORD' :  '1234',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
 }
 
 
